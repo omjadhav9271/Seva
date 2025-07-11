cbccc4cb3e9d5d3b1a467f7738cf12435fbe3368
Om Jadhav <omjadhav9271@gmail.com>
2025-07-11 13:09:30 +0530

    .

--- my_app/app/layout.tsx
+++ my_app/app/layout.tsx
@@ -1,10 +1,15 @@
+import '@/app/ui/global.css';
+
+// Define the RootLayout component which wraps all pages
 export default function RootLayout({
-  children,
+  children, // React nodes passed as children to be rendered inside the layout
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode; // Type definition for children prop
 }) {
   return (
+    // Set the language attribute for the HTML document
     <html lang="en">
+      {/* Render the children inside the body tag */}
       <body>{children}</body>
     </html>
   );
